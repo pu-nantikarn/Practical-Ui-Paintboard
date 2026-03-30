116a57196e1444a0d1033f9743c5cc49ec6859fb
pu-nantikarn <pu.nantikarn@gmail.com>
2026-03-31 03:39:39 +0700

    fixed find url

--- src/backend/supabaseClient.js
+++ src/backend/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// สั่งให้ดึงค่าความลับมาจากไฟล์ .env
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 

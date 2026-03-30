305c8147a1c6ab1d277f878e503ecfe737f95e3a
pu-nantikarn <pu.nantikarn@gmail.com>
2026-03-31 03:31:07 +0700

    Add .env to gitignore for security

--- src/backend/supabaseClient.js
+++ src/backend/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ค่า 2 ตัวนี้เอามาจาก Supabase -> Project Settings -> API
-const supabaseUrl = 'https://fusfddvfjhdfbbnznofg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1c2ZkZHZmamhkZmJibnpub2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NjI3MzgsImV4cCI6MjA4OTQzODczOH0.rRoPT605NLwkoCjbrs-to8EFJNPc2Jkq1Es5UtLz5uk';
+// สั่งให้ดึงค่าความลับมาจากไฟล์ .env
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
